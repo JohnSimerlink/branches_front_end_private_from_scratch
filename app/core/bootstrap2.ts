c22cadd67ad5999a3b4cfdf83a18651f23bf5448
JohnSimerlink <johnsimerlink@gmail.com>
2018-10-13 17:41:19 -0400

    show that async and object rest spread working

--- app/core/bootstrap2.ts
+++ app/core/bootstrap2.ts
@@ -5,6 +5,23 @@
 // import {TYPES} from '../objects/types';
 
 console.log('bootstrap2.ts called')
+async function f() {
+	console.log(`now is ${Date.now()}`)
+	const age =	await new Promise((resolve, reject) => {
+		setTimeout(function(){
+			console.log("delayed call")
+			resolve(22)
+		}, 1000)
+	})
+	console.log(`after is ${Date.now()}`)
+	const obj1 = {a: 3, b: 4}
+	const obj2 = {
+		...obj1,
+		c: 5,
+	}
+	console.log(`obj2 is ${obj2}`, obj2)
+}
+f()
 // myContainerLoadAllModules({fakeSigma: false});
 // const appContainer = myContainer.get<AppContainer>(TYPES.AppContainer);
 // appContainer.start();
